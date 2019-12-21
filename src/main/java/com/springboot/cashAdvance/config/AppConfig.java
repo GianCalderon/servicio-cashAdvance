4c7790f74d068b9672588ade624e3f808f690201
GianCalderon <gian.010692@gmail.com>
2019-12-20 19:29:41 -0500

    actualizado al 20/12

--- src/main/java/com/springboot/cashAdvance/config/AppConfig.java
+++ src/main/java/com/springboot/cashAdvance/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.springboot.cashAdvance;
+package com.springboot.cashAdvance.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
